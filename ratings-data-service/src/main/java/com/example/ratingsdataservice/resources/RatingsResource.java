cf872a57ec5fdf9f3c77c569496041c51a3e6664
joelo100 <73218455+joelo100@users.noreply.github.com>
2020-12-10 15:10:14 +0800

    Add file via upload

---
+++ ratings-data-service/src/main/java/com/example/ratingsdataservice/resources/RatingsResource.java
@@ -0,0 +1,32 @@
+package com.example.ratingsdataservice.resources;
+
+import java.util.Arrays;
+import java.util.List;
+
+import com.example.ratingsdataservice.model.Rating;
+import com.example.ratingsdataservice.model.UserRating;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ratingdata")
+public class RatingsResource {
+    
+    @RequestMapping("/{movieId}")
+    public Rating getRating(@PathVariable("movieId") String movieId) {
+        return new Rating(movieId, 4);
+    }
+
+    @RequestMapping("users/{userId}")
+    public UserRating getUserRating(@PathVariable("userId") String userId) {
+        List<Rating> ratings = Arrays.asList(
+            new Rating("1234", 4), 
+            new Rating("5678", 3)
+        );
+        UserRating userRating = new UserRating(); 
+        userRating.setUserRating(ratings);
+          return userRating;   
+    }
+}
